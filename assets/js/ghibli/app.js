5cf928ed4c47c6c96c1763ef60e0557d2c92ef7f
b-richard <benjamin.richard-exterieur@fiducial.net>
2017-11-15 10:41:46 +0100

    add quasar fwk (not using it's own cli coz we are in a Sf3+ environments), pb is that we don't have sccs files and so only dist css files that overload our bootstrap scss

--- assets/js/ghibli/app.js
+++ assets/js/ghibli/app.js
@@ -4,14 +4,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import Loader from './components/Loader.vue'
 import router from './router'
+import Quasar from 'quasar-framework'
+
+require('../../../node_modules/quasar-framework/dist/quasar.mat.css')
+// not sure that fonts are loaded finely
+require('quasar-extras/roboto-font')
+import 'quasar-extras/material-icons'
+// import 'quasar-extras/ionicons'
+// import 'quasar-extras/fontawesome'
+// import 'quasar-extras/animate'
 
 Vue.config.productionTip = false
 Vue.component('Loader', Loader)
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    template: '<App/>',
-    components: { App },
+Quasar.start(() => {
+    /* eslint-disable no-new */
+    new Vue({
+        el: '#app',
+        router,
+        template: '<App/>',
+        components: {App},
+    })
 })
