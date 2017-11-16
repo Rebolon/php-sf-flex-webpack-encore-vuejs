cd6a6db2395e473f62a275226f641f578a331c00
b-richard <benjamin.richard-exterieur@fiducial.net>
2017-11-16 15:17:57 +0100

    add the logger to demonstrate the autowire into SimplerController

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
