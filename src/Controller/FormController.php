f9929860800c651d84b9cf35613c96ebf84aaa40
b-richard <benjamin.richard-exterieur@fiducial.net>
2017-11-22 14:11:18 +0100

    fix csrf usage in symfony form @todo : use controller as a service to re-use the token generation and the token validation actions
    @todo use it in vuejs form

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FormController extends Controller
@@ -26,7 +32,7 @@ class FormController extends Controller
     /**
     * @Route("/login", name="login")
     */
-    public function login(Request $request, AuthenticationUtils $authUtils)
+    public function login(Request $request, AuthenticationUtils $authUtils, CsrfTokenManagerInterface $tokenManager)
     {
         // get the login error if there is one
         $error = $authUtils->getLastAuthenticationError();
@@ -36,9 +42,9 @@ class FormController extends Controller
 
         /** my own code for csrf coz crsf_token() twig helper doesn't exists in my stack, why ??? **/
         /* don't use container, send params by DI */
-        $tokenManager = $this->get('security.csrf.token_manager');
-        $inception = $this->container->getParameter('crsf_inscription_inception');
-        $token = $tokenManager->getToken($inception);
+        // $tokenManager = $this->get('security.csrf.token_manager');
+        $tokenId = $this->getParameter('csrf_token_id');
+        $token = $tokenManager->getToken($tokenId);
 
         return $this->render('form/login.html.twig', array(
             'last_username' => $lastUsername,
@@ -47,17 +53,37 @@ class FormController extends Controller
         ));
     }
 
-    public function checkToken(Request $request,) {
-        // must be passed as method params
-        $tokenManager = $this->get('security.csrf.token_manager');
-        // must be passed as method param
-        $tokenId = $this->container->getParameter('crsf_inscription_inception');
+    /**
+     * @Route("/token", name="token")
+     * @param Request $request
+     * @param ParameterBag $params
+     * @return JsonResponse
+     */
+    public function token(Request $request, ParameterBag $params) {
+        $tokenId = $params->get('csrf_token_id');
         $tokenValue = $request->get('token');
         $token = new CsrfToken($tokenId, $tokenValue);
+
+        return new JsonResponse(['token' => $token->getValue(), ]);
+    }
+
+    /**
+     * @Route("/token_check", name="token")
+     * @param Request $request
+     * @param CsrfTokenManagerInterface $tokenManager
+     * @param Parameter $params
+     * @return Response
+     */
+    public function tokenCheck(Request $request, CsrfTokenManagerInterface $tokenManager, ParameterBag $params) {
+        // @todo use the token action, decode the json and use it here instead of doing the same thing
+        $tokenId = $params->get('csrf_token_id');
+        $tokenValue = $request->get('token');
+        $token = new CsrfToken($tokenId, $tokenValue);
+
         if (!$tokenManager->isTokenValid($token)) {
             throw new HttpException(400, 'Invalid token');
         }
 
-        return true;
+        return new Response();
     }
 }
