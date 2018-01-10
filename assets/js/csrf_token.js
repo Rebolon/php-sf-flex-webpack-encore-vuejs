37eddc219fa9f879ab0143a40929d44b061add31
b-richard <benjamin.richard-exterieur@fiducial.net>
2018-01-10 09:45:51 +0100

    fix login/logout process with fetch (should use axios lib with a middleware to take care of csrftoken and logout)

--- assets/js/csrf_token.js
+++ assets/js/csrf_token.js
@@ -4,35 +4,39 @@ export default function getToken(loaderToActivate)
     let metaContent = meta ? meta.getAttribute('content') : ''
 
     return new Promise((resolve, reject) => {
-        if (metaContent) {
-            resolve(metaContent)
-        } else {
-            loaderToActivate.isLoading = true
-            const uri = '/token'
-            const myHeaders = new Headers()
-            myHeaders.append("Accept", "application/json")
-            myHeaders.append("Content-Type", "application/json")
-            const myInit = {
-              method: 'GET',
-              headers: myHeaders,
-              credentials: 'same-origin',
-              mode: 'cors',
-              cache: 'no-cache',
-            }
-            fetch(uri, myInit)
-            // @todo manage http error
-                .then(res => res.json())
-                .then(res => {
-                    metaContent = res
-                    if (!meta) {
-                        meta = document.createElement('meta')
-                        meta.setAttribute('name', 'csrf_token')
-                        document.querySelector('head').appendChild(meta)
-                    }
-                    meta.setAttribute('content', res)
-                    loaderToActivate.isLoading = false
-                    resolve(metaContent)
-                })
+        if (loaderToActivate) {
+          loaderToActivate.isLoading = true
         }
+
+        const uri = '/token'
+        const myHeaders = new Headers()
+        myHeaders.append("Accept", "application/json")
+        myHeaders.append("Content-Type", "application/json")
+        const myInit = {
+          method: 'GET',
+          headers: myHeaders,
+          credentials: 'same-origin',
+          mode: 'cors',
+          cache: 'no-cache',
+        }
+        fetch(uri, myInit)
+            // @todo manage http error
+            .then(res => res.json())
+            .then(res => {
+                changeMeta(meta, res)
+                if(loaderToActivate) {
+                    loaderToActivate.isLoading = false
+                }
+                resolve(res)
+            })
     })
+}
+
+const changeMeta = function(meta, token) {
+  if (!meta) {
+    meta = document.createElement('meta')
+    meta.setAttribute('name', 'csrf_token')
+    document.querySelector('head').appendChild(meta)
+  }
+  meta.setAttribute('content', token)
 }
