865d73545127d89ade7ada367bca22d1560107c1
b-richard <benjamin.richard-exterieur@fiducial.net>
2018-05-15 09:38:28 +0200

    add 2 small TU for Commands

--- tests/Command/DumpJsConfig.php
+++ tests/Command/DumpJsConfig.php
@@ -10,11 +10,34 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class DumpJsConfig extends WebTestCase
 {
+    protected function setUp()
+    {
+        $this->router = $this->createMock('\Symfony\Component\Routing\RouterInterface');
+        $this->twig = $this->createMock('\Twig_Environment');
+    }
+
     /**
      * @group git-pre-push
      */
-    public function testDumpJsConfig()
+    public function testDumpJsConfigInfo()
     {
-        $this->markTestIncomplete('Should test command');
+        $csrfTokenParameter = 'csrfToken';
+        $apiPlatformPrefix = '/api';
+        $loginUsernamePath = 'login';
+        $loginPasswordPath = 'pwd';
+
+        $tested = new \App\Command\DumpJsConfig($csrfTokenParameter, $apiPlatformPrefix, $loginUsernamePath, $loginPasswordPath, $this->twig, $this->router);
+        $this->assertTrue($tested->isEnabled(), 'Should be enabled');
+        $this->assertEquals($tested->getName(), 'app:dump-js-config');
+        $this->assertEquals($tested->getDescription(), 'Create the config.js file.');
+        $this->assertEquals($tested->getHelp(), 'Dump symfony configuration into a config.js file available from assets/js/*');
+    }
+
+    /**
+     * @group git-pre-push
+     */
+    public function testDumpJsConfigRun()
+    {
+        $this->markTestIncomplete('Should test run command, but needs to change the filepath of the js file or it will be overwritten by test');
     }
 }
--- tests/Command/GetJwtToken.php
+++ tests/Command/GetJwtToken.php
@@ -10,11 +10,32 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class GetJwtToken extends WebTestCase
 {
+    protected function setUp()
+    {
+        $this->logger = $this->createMock('\Psr\Log\LoggerInterface');
+        $this->provider = $this->createMock('\Symfony\Component\Security\Core\User\InMemoryUserProvider');
+        $this->encoder = $this->createMock('\Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface');
+        $this->passwordEncoder = $this->createMock('\Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface');
+        $this->tokenTools = $this->createMock('\App\Security\JwtTokenTools');
+    }
+
     /**
      * @group git-pre-push
      */
-    public function testGetJwtToken()
+    public function testDumpJsConfigInfo()
     {
-        $this->markTestIncomplete('Should test command');
+        $tested = new \App\Command\GetJwtToken($this->logger, $this->provider, $this->encoder, $this->passwordEncoder, $this->tokenTools);
+        $this->assertTrue($tested->isEnabled(), 'Should be enabled');
+        $this->assertEquals($tested->getName(), 'app:new-jwt-token');
+        $this->assertEquals($tested->getDescription(), 'Generate a new valid JWT token');
+        $this->assertEquals($tested->getHelp(), 'Generate a new valid JWT token, to use with API for example.');
+    }
+
+    /**
+     * @group git-pre-push
+     */
+    public function testDumpJsConfigRun()
+    {
+        $this->markTestIncomplete('Should test run command');
     }
 }
